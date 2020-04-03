94e45e0b6b25e194f65586d535a6432b9a90c5e0
Anthony Heathcoat <aeheathc@example.com>
2020-04-03 15:30:39 -0400

    Added commandline config for setting conf file

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,9 +1,10 @@
-extern crate config;
+extern crate clap;
 
 use std::net::TcpListener;
 use std::env;
 use std::path::Path;
 use threadpool::ThreadPool;
+use clap::{Arg, App};
 
 use c20web::handle_connection;
 use c20web::SETTINGS;
@@ -11,13 +12,25 @@ use c20web::DEFAULT_CONFIG;
 
 fn main()
 {
-	let working_dir = Path::new("data");
-	env::set_current_dir(&working_dir).expect("Couldn't set cwd");
+    let matches = App::new("c20web")
+                          .version("1.0.0-dev")
+                          .about("Very simple web server")
+                          .arg(Arg::with_name("working_dir")
+                               .short("d")
+                               .long("workingdir")
+							   .help("Working directory. Will look here for the config file (web.toml) and will be the base for all relative paths used elsewhere in the config. Defaults to ./data for easy dev use with `cargo run` but an absolute path is recommended otherwise.")
+							   .default_value("data")
+                               .takes_value(true))
+                          .get_matches();
+
+	let working_dir = matches.value_of("working_dir").unwrap();
+	env::set_current_dir(Path::new(working_dir)).expect("Couldn't set cwd");
 
 	let (threads_max,listen_addr): (usize,String) = {
 		let mut settings = SETTINGS.write().expect("Couldn't get config in main");
 		settings.merge(config::File::from_str(&DEFAULT_CONFIG, config::FileFormat::Toml)).expect("Couldn't merge default config");
 		settings.merge(config::File::with_name("web")).expect("Couldn't merge config from file");
+		settings.set("working_dir",working_dir).expect("Couldn't merge config from commandline");
 
 		(
 			settings.get::<usize>("threads_max").expect("threads_max missing from config"),
